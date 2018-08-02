88f522c0892d0a2593b6a52c6ec9199528ae3b28
edward <L_zejie@163.com>
2018-08-02 16:47:54 +0800

    [update]更新文档以及调用方式

--- TextEncoder/transformer.py
+++ TextEncoder/transformer.py
@@ -12,8 +12,19 @@ import jieba
 
 
 class Transformer(object):
-    '''
+    """
     将文本转化为索引序号，默认采用jiaba.cut作为分词方式，如果想采用字的方式，则使用list或自定义分词方式
+
+    >>> t = Transformer(cutter=list, use_placeholder=True)
+    >>> ss = ['abcde', 'cdefgg', 'scdea']
+    >>> t.feed(ss)
+    >>> isinstance(t.text_to_index('aacds'), list) and len(t.text_to_index('aacds')) > 0
+    True
+    >>> isinstance(t('aacds'), list) and len(t('aacds')) > 0
+    True
+    >>> indexs = t('as12356df')
+    >>> t.index_to_token(indexs)
+    ['a', 's', '@', '@', '@', '@', '@', 'd', 'f']
 
     >>> t = Transformer(cutter=list, use_placeholder=False)
     >>> ss = ['abcde', 'cdefgg', 'scdea']
@@ -22,12 +33,10 @@ class Transformer(object):
     True
     >>> isinstance(t('aacds'), list) and len(t('aacds')) > 0
     True
-    >>> t('')
-    []
-    >>> indexs = t('asdf')
-    >>> t.index_to_text(indexs)
-    'asdf'
-    '''
+    >>> isinstance(t(''), list) and len(t('')) == 0
+    True
+    """
+
     def __init__(
             self,
             cutter=jieba.cut,
@@ -40,6 +49,19 @@ class Transformer(object):
             UNK='@',
             use_placeholder=True,
     ):
+        '''
+        初始化文本编码器转化器
+
+        :param cutter: 分词器
+        :param max_length: 文本最大长度
+        :param token_frequence: 最小出现频次
+        :param pretreat: 文本预处理函数
+        :param stop_words: 停用词
+        :param STR: 开始标识符
+        :param END: 结尾标识符
+        :param UNK: 未知标识符
+        :param use_placeholder: 是否使用标识符True/False
+        '''
         # 分词器
         self.cutter = cutter
         # 文本最大长度
@@ -72,38 +94,64 @@ class Transformer(object):
         return self.text_to_index(text)
 
     def feed(self, texts):
+        '''
+        传入语料
+
+        :param texts: 语料列表
+        '''
         for line in texts:
             for token in set(self.transform_2_words(line)):
                 self.tokens[token] += 1
-        leave_tokens = [item[0]
-            for item in sorted(
+        leave_tokens = [
+            item[0] for item in sorted(
                 filter(
                     lambda x: x[1] > self.token_frequence,
                     self.tokens.items()
                 ), key=lambda x: -x[1]
             )
         ]
+        leave_tokens = [self.STR, self.END, self.UNK] + leave_tokens
         self.token_index = dict(zip(leave_tokens, range(len(leave_tokens))))
         self.index_token = dict(zip(range(len(leave_tokens)), leave_tokens))
 
-    def transform_2_words(self, line):
-        new_line = self.STR + self.pretreat(line) + self.END
+    def transform_2_words(self, text):
+        '''
+        文本预处理入口
+        :param text: 待处理文本
+        :return: str
+        '''
+        new_line = self.STR + self.pretreat(text) + self.END
         tokens = self.cutter(new_line)
         return tokens[:self.max_length]
 
-    def token_encode(self, tokens):
-        assert isinstance(tokens, list), 'token必须为列表'
-        return [self.token_index.get(token, self.UNK) for token in tokens]
-
     def text_to_index(self, text):
-        return self.token_encode(self.transform_2_words(text))
+        '''
+        文本转化为索引
+        :param text: 待转化文本
+        :return: [int, int, ... , int]
+        '''
+        return self.token_to_index(self.transform_2_words(text))
 
-    def index_to_text(self, indexs):
-        return ''.join([
-            self.index_token[index]
+    def token_to_index(self, tokens):
+        '''
+        token列表转化为索引
+        :param tokens: 待转化token列表
+        :return: [int, int, ... , int]
+        '''
+        filted_tokens = [token if token in self.tokens else self.UNK for token in tokens]
+        return [self.token_index[token] for token in filted_tokens]
+
+    def index_to_token(self, indexs):
+        '''
+        索引转化为token
+        :param indexs: 索引列表
+        :return: [str, str, ... , str]
+        '''
+        return [
+            self.index_token.get(index, self.UNK)
             for index in indexs
-            if self.index_token[index] not in [self.STR, self.END, self.UNK]
-        ])
+            if self.index_token.get(index) not in [self.STR, self.END] and self.index_token.get(index)
+        ]
 
 
 if __name__ == '__main__':
